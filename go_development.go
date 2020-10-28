432530f29808bcd033f9455ae66b06f5235e927b
edm20627 <eng00627@gmail.com>
2020-10-28 13:45:30 +0900

    加筆

--- go_development.go
+++ go_development.go
@@ -1,11 +1,26 @@
 package main
 
 import (
-	"fmt"
 	"flag"
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
-var version = "1.0.0"
+var version = "0.0.0"
+
+type MySignal struct {
+	message string
+}
+
+func (s MySignal) String() string {
+	return s.message
+}
+
+func (s MySignal) Signal() {}
 
 func main() {
 	var showVersion bool
@@ -16,5 +31,28 @@ func main() {
 	if showVersion {
 		fmt.Println("version:", version)
 		return
+	}
+
+	log.Println("[info] Start")
+	trapSingals := []os.Signal{
+		syscall.SIGHUP,
+		syscall.SIGINT,
+		syscall.SIGTERM,
+		syscall.SIGQUIT}
+
+	sigCh := make(chan os.Signal, 1)
+
+	time.AfterFunc(5*time.Second, func() {
+		sigCh <- MySignal{"time out"}
+	})
+
+	signal.Notify(sigCh, trapSingals...)
+
+	sig := <-sigCh
+	switch s := sig.(type) {
+	case syscall.Signal:
+		log.Printf("[info] Got signal: %s(%d)", s, s)
+	case MySignal:
+		log.Printf("[info] %s", s)
 	}
 }
