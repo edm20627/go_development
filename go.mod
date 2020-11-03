31fca291ffa27027ff4456906781370f787309e4
edm20627 <eng00627@gmail.com>
2020-11-04 01:23:08 +0900

    restサーバー追加

--- go.mod
+++ go.mod
@@ -4,10 +4,15 @@ go 1.15
 
 require (
 	github.com/go-gorp/gorp v2.2.0+incompatible
+	github.com/go-playground/universal-translator v0.17.0 // indirect
+	github.com/labstack/echo v3.3.10+incompatible
+	github.com/labstack/gommon v0.3.0 // indirect
+	github.com/leodido/go-urn v1.2.0 // indirect
 	github.com/lib/pq v1.8.0
 	github.com/mattn/go-isatty v0.0.12
 	github.com/mitchellh/cli v1.1.2 // indirect
 	github.com/sirupsen/logrus v1.7.0
 	golang.org/x/lint v0.0.0-20200302205851-738671d3881b // indirect
 	golang.org/x/tools v0.0.0-20201023174141-c8cfbd0f21e6 // indirect
+	gopkg.in/go-playground/validator.v9 v9.31.0
 )
--- go_development.go
+++ go_development.go
@@ -1,11 +1,48 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"fmt"
 	"io"
+
+	"log"
+	"net/http"
 	"os"
+	"time"
+
+	"github.com/go-gorp/gorp"
+	"github.com/labstack/echo"
+	_ "github.com/lib/pq"
+	"gopkg.in/go-playground/validator.v9"
 )
+
+type Comment struct {
+	Id      int64     `json:"id" db:"id,primarykey,autoincrement"`
+	Name    string    `json:"name" db:"name,notnull,size:200" validate:"required,max=200"`
+	Text    string    `json:"text" db:"text,notnull,size:399" validate:"required,max=399"`
+	Created time.Time `json:"created" db:"created,notnull"`
+	Updated time.Time `json:"updated" db:"updated,notnull"`
+}
+
+func (c *Comment) PreInsert(s gorp.SqlExecutor) error {
+	c.Created = time.Now()
+	c.Updated = c.Created
+	return nil
+}
+
+func (c *Comment) PreUpdate(s gorp.SqlExecutor) error {
+	c.Updated = time.Now()
+	return nil
+}
+
+type Validator struct {
+	validator *validator.Validate
+}
+
+func (v *Validator) Validate(i interface{}) error {
+	return v.validator.Struct(i)
+}
 
 const (
 	ExitCodeOK int = iota
@@ -20,6 +57,55 @@ type CLI struct {
 }
 
 func main() {
+	dsn := "host=postgres user=user dbname=app_db password=password sslmode=disable"
+	db, err := sql.Open("postgres", dsn)
+	if err != nil {
+		log.Fatal(err)
+	}
+	dbmap := &gorp.DbMap{Db: db, Dialect: gorp.PostgresDialect{}}
+	dbmap.AddTableWithName(Comment{}, "comments")
+	err = dbmap.CreateTablesIfNotExists()
+
+	e := echo.New()
+	e.Validator = &Validator{validator: validator.New()}
+
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello")
+	})
+
+	e.Static("/", "static/")
+
+	e.GET("/api/comments", func(c echo.Context) error {
+		var comments []Comment
+		_, err := dbmap.Select(&comments,
+			"select * from comments order by created desc limit 10")
+		if err != nil {
+			c.Logger().Error("Select: ", err)
+			return c.String(http.StatusBadRequest, "Select: "+err.Error())
+		}
+		return c.JSON(http.StatusOK, comments)
+	})
+
+	e.POST("/api/comments", func(c echo.Context) error {
+		var comment Comment
+		if err := c.Bind(&comment); err != nil {
+			c.Logger().Error("Bind: ", err)
+			return c.String(http.StatusBadRequest, "Bind: "+err.Error())
+		}
+		if err = c.Validate(&comment); err != nil {
+			c.Logger().Error("Validate: ", err)
+			return c.String(http.StatusBadRequest, "Validate: "+err.Error())
+		}
+		if err = dbmap.Insert(&comment); err != nil {
+			c.Logger().Error("Insert: ", err)
+			return c.String(http.StatusBadRequest, "Insert: "+err.Error())
+		}
+		c.Logger().Info("ADDED: %v", comment.Id)
+		return c.JSON(http.StatusCreated, "")
+	})
+
+	e.Logger.Fatal(e.Start(":8080"))
+
 	cli := &CLI{outStream: os.Stdout, errStream: os.Stderr}
 	os.Exit(cli.Run(os.Args))
 }
